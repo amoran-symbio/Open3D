b8a0a5070a979e672ef33d55a57cde69dfb7d409
Yixing Lao <yixing.lao@gmail.com>
2022-01-21 22:02:50 -0800

    use locally-build filament

--- 3rdparty/filament/filament_precompile.sh
+++ 3rdparty/filament/filament_precompile.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1-linux-x86_64.tar.gz"
 FILAMENT_URL="https://github.com/isl-org/filament/archive/fcd2930eb75924bbb7afbe990de9782af4b5d1dc.tar.gz"
-FILAMENT_TAR="filament-fcd2930eb75924bbb7afbe990de9782af4b5d1dc-linux-amd64.tgz"
+FILAMENT_TAR="filament-fcd2930eb75924bbb7afbe990de9782af4b5d1dc-linux-amd64.tar.gz"
 
 pushd "${SCRIPT_DIR}"
 
